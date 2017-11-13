45253a6d5d681f75330bf0bcbdc87751324740b1
Atem Matiashov <artem.matiashov@netcracker.com>
2017-11-13 14:22:34 +0300

    Add method for get info about ssh keys

--- main.py
+++ main.py
@@ -94,17 +94,30 @@ def start_server(ctid):
     print(response.text)
 
 
+def get_ssh_key_info():
+    uri = config["base_api_uri"] + config["sshkeys"]
+    response = requests.get(uri, headers=request_header)
+    if response.status_code == requests.codes.ok:
+        return {"status_code": response.status_code,
+                "body": response.json()}
+    elif response.status_code == 403:
+        logger.error("get_general_info Forbidden")
+        return {"status_code": response.status_code,
+                "body": None}
+    print(response.text)
+
 # account = get_general_info()
 # servers = get_servers()
 # vs01 = get_server_details(95116)
 # restart = restart_server(95116)
 # stop = stop_server(95116)
-start = start_server(95116)
-
+# start = start_server(95116)
+sshkeys = get_ssh_key_info()
 
 # print(json.dumps(account, indent=4, ensure_ascii=False))
 # print(json.dumps(servers, indent=4, ensure_ascii=False))
 # print(json.dumps(vs01, indent=4, ensure_ascii=False))
 # print(json.dumps(restart, indent=4, ensure_ascii=False))
 # print(json.dumps(stop, indent=4, ensure_ascii=False))
-print(json.dumps(start, indent=4, ensure_ascii=False))
+# print(json.dumps(start, indent=4, ensure_ascii=False))
+print(json.dumps(sshkeys, indent=4, ensure_ascii=False))
